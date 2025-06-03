33fb4c7047782e5d99ebf60f781f6f5d78c94ba3
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2025-06-03 13:27:50 +0200

    vis modal før konsument opprettes

--- src/main/kotlin/konsument/KonsumentForm.kt
+++ src/main/kotlin/konsument/KonsumentForm.kt
@@ -6,8 +6,11 @@ import no.nav.pam.stilling.feed.admin.komponenter.Input
 
 fun FlowContent.KonsumentForm() {
     form {
+        id = "konsumentForm"
         attributes["hx-post"] = "/konsument/opprett"
         attributes["hx-target"] = "#konsument"
+        attributes["hx-trigger"] = "submit"
+        attributes["hx-indicator"] = "#laster"
 
         Input {
             name = "identifikator"
@@ -15,6 +18,10 @@ fun FlowContent.KonsumentForm() {
             type = InputType.text
             required = true
             autoFocus = true
+            attributes["_"] = "on input set global heading.value to " +
+                              "`Opprett konsument: \${my value}` " +
+                              "then set global body.value to " +
+                              "`Er du sikker på at du vil opprette og generere token for kosumenten: \${my value}?` "
         }
 
         Input {
@@ -39,8 +46,18 @@ fun FlowContent.KonsumentForm() {
         }
 
         Button {
-            type = ButtonType.submit
+            id = "modalButton"
+            attributes["hx-get"] = "/modal"
+            attributes["hx-target"] = "#konsumentForm"
+            attributes["hx-swap"] = "beforeend"
+            attributes["hx-include"] = "#modalContent"
+            attributes["hx-indicator"] = "#modalContent"
             label = "Legg til konsument"
+        }
+        span {
+            id = "modalContent"
+            input(type = InputType.hidden, name = "heading") { attributes["_"] = "on load set global heading to me" }
+            input(type = InputType.hidden, name = "body") { attributes["_"] = "on load set global body to me" }
         }
     }
 }
