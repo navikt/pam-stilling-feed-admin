087fe82e5103cd18c8c166a82a683e1de8203562
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2025-06-04 09:58:05 +0200

    skriveleif

--- src/main/kotlin/token/GenererTokenForm.kt
+++ src/main/kotlin/token/GenererTokenForm.kt
@@ -30,7 +30,7 @@ fun FlowContent.GenererTokenForm(
                 attributes["_"] = "on load or change log my.selectedOptions.innerHTML[0] then set global heading.value to " +
                         "`Generer nytt token for konsument: \${my.selectedOptions.innerHTML[0]}`" +
                         "then set global body.value to " +
-                        "`Er du sikker på at du vil generere et nytt token for kosumenten: \${my.selectedOptions.innerHTML[0]}? Denne handlingen invalidere det eksisterende tokenet, og kan ikke angres.`"
+                        "`Er du sikker på at du vil generere et nytt token for kosumenten: \${my.selectedOptions.innerHTML[0]}? Denne handlingen vil invalidere det eksisterende tokenet, og kan ikke angres.`"
                 konsumenter.forEach { konsument ->
                     option {
                         value = konsument.id.toString()
